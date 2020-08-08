863f61ccd1b79e920256152418fcbae19131c408
Akimasa Liu <liumingzheng8@outlook.com>
2020-08-08 13:15:12 +0900

    Add setItem,getItem etc...

--- BIT.py
+++ BIT.py
@@ -10,34 +10,46 @@ class BIT:
     def update(self,i,j):
         k=j-self.l[i]
         e=i
-        while e<self.n+1:
+        while e<self.n+1:#1-indexed
             self.l[e]+=k
             e+=self.lsb(e)
-        return 
     def sum(self,right,left=None):
         if left:
             return self.sum(left)-self.sum(right-1)
+        self.raiseIndexError(right)
         ans=0
         while right:
             ans+=self.l[right]
             right-=self.lsb(right)
         return ans
     def __iter__(self):
-        return iter(self.l)
+        for i in self.l:
+            yield i
     def __str__(self):
         return str(list(self))
+    def __setitem__(self,key,value):
+        self.raiseIndexError(key)
+        self.update(key,value)
+    def __getitem__(self,key):
+        self.raiseIndexError(key)
+        return self.l[key]
+    def isIndexError(self,key):
+        return key>self.n+1
+    def raiseIndexError(self,key):
+        if self.isIndexError(key):
+            raise IndexError('list index out of range')
 
 def checkBIT():
     b=BIT(10)
     print(b)
     for i in range(int(input())):
-        u,t,f=input().split()
-        t,f=map(int,(t,f))
-        if u=='u':
-            b.update(t,f)
+        u,t,f=map(int,input().split())
+        if u==1:
+            b[t]=f
         else:
             print(b.sum(t,f))
-    print(b)
+    print(b,b[2])
+    print(b.isIndexError(100))
 
 def main():
     checkBIT()
