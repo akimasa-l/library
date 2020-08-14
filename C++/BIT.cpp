3abc228953c624e1e2f8a452d70effb10f495a7d
Akimasa Liu <liumingzheng8@outlook.com>
2020-08-14 18:34:57 +0900

    add C++ BIT

--- C++/BIT.cpp
+++ C++/BIT.cpp
@@ -3,26 +3,38 @@
 using namespace std;
 
 
-//出来てない
-/*
+using ll=long long;
+
 template<class T> struct BIT{
 using VT=vector<T>;
 T n;
-private:
 VT l;
-public:
-BIT(const T& num){
-    T init_l=0;
-    BIT(num,init_l);
+BIT(const T& num):n(num),l(n+1,0){}
+BIT(const T& num,const T& init_l):n(num),l(n+1,init_l){}
+T lsb(T i){return i&(-i);}
+T add(T i, T j){//add j to l_i
+T e=i;
+while(e<n+1){
+    l.at(e)+=j;
+    e+=lsb(e);
 }
-BIT(const T& num,const T& init_l){
-    n=num;
-    l(n+1,init_l);
-    return;
+return j
+}
+T update(T i,T j){//update j to l_i
+    return add(i,j-l.at(i))+j;
+}
+ll sum(T le,T ri){
+    return sum(ri)-sum(le-1);
+}
+ll sum(T le){
+    ll ans=0;
+    while(le>0){
+        ans+=l.at(le);
+        le-=lsb(le);
+    }
+    return ans;
 }
 };
 int main(){
-    auto b=BIT<int>(3);
-    cout<<b.l.at(0)<<endl;
+    
 }
-*/
