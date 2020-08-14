2be99fcbd17f20c0ecc23270cb4e131d8f70257f
Akimasa Liu <liumingzheng8@outlook.com>
2020-08-14 12:13:44 +0900

    add Union-find.py

--- C++/BIT.cpp
+++ C++/BIT.cpp
@@ -3,8 +3,26 @@
 using namespace std;
 
 
+//出来てない
+/*
 template<class T> struct BIT{
+using VT=vector<T>;
 T n;
 private:
-vector<T> l(n+1,0);
+VT l;
+public:
+BIT(const T& num){
+    T init_l=0;
+    BIT(num,init_l);
+}
+BIT(const T& num,const T& init_l){
+    n=num;
+    l(n+1,init_l);
+    return;
+}
 };
+int main(){
+    auto b=BIT<int>(3);
+    cout<<b.l.at(0)<<endl;
+}
+*/
