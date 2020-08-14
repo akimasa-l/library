883f1176b568560835e8cee6139601de136a116b
Akimasa Liu <liumingzheng8@outlook.com>
2020-08-14 14:50:02 +0900

    Completely understand Union-Findgit status

---
+++ python/Union-Find.py
@@ -0,0 +1,19 @@
+class UnionFind:
+    def __init__(self,n):
+        self.l=list(range(n))
+        self.n=n
+        self.rank=[0 for i in range(n)]
+    def find(self,x):
+        if self.l[x]==x:
+            return x
+        self.l[x]=self.find(self.l[x])
+        return self.l[x]
+    def union(self,x,y):
+        if x==y:return
+        if self.rank[x]>self.rank[y]:
+            return self.union(y,x)
+        a,b=self.find(x),self.find(y)
+        self.l[a]=b
+        self.rank[a]=max(self.rank[b]+1,self.rank[a])
+    def isSame(self,x,y):
+        return self.find(x)==self.find(y)
